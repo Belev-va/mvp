44d6b3a6bebd466de406761fc39bc5d01deea526
v.belev <v.belev@sdgroup.ai>
2024-12-25 02:42:02 +0400

    Initial commit: Microservices project setup9

--- init-db.sh
+++ init-db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+psql -v ON_ERROR_STOP=1 --username "postgres" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE profile_db;
     CREATE DATABASE project_db;
 EOSQL
